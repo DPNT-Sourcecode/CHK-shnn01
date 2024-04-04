1852df582c3468b0dd6c21b0ddb0e7438fe55da1
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 06:25:00 +0000

    Thu Apr 04 06:25:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -24,12 +24,17 @@ def checkout(skus:str)->int:
     skus = Counter(skus)
     for item in skus:
         if item in discounts and (skus[item]%discounts[item][0]==0):
+            # caveat: this puts the condition that the discount will only be applied if the ordered quantity is a perfect multiple of 
+            # of the quantity specified in the discount. 
             item_group_count = skus[item] / discounts[item][0]
-            item_price = item_price
+            item_price = item_group_count * discounts[item][0]
             full_price += item_price
+        elif item in prices:
+            full_price += skus[item] prices[item]
     # scenario where no valid value is entered
     checkout += 0
     return 
+
 
 
 
