e01f335dfd8dc821ade92503322b8dd5e52125d9
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 06:13:00 +0000

    Thu Apr 04 06:13:00 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -2,6 +2,20 @@
 
 # noinspection PyUnusedLocal
 # skus = unicode string
-def checkout(skus:str):
-    # find out structure of string
-    return skus
+"""
+Our price table and offers: 
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+| B    | 30    | 2B for 45      |
+| C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
+
+"""
+prices = {"A": 50, "B": 30, "C": 20, "D": 15,}
+def checkout(skus:str)->int:
+    
+    return 
+
