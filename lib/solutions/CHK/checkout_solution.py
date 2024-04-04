d58fabc0d541a9d53ab556d4cc40e7fab0634c66
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 07:08:33 +0000

    Thu Apr 04 07:08:33 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,13 +18,6 @@ from collections import Counter
 prices = {"A": 50, "B": 30, "C": 20, "D": 15,}
 discounts = {"A": (3, 130), "B": (2, 45), }
 
-def apply_discount(on_offer:dict)->tuple[int, dict]:
-    for item in on_offer:
-        item_price = item_group_count * discounts[item][0]
-            full_price += item_price
-
-
-
 def checkout(skus:str)->int:
     full_price = 0
 
@@ -33,13 +26,15 @@ def checkout(skus:str)->int:
     
     to_discount = {}
     normal_price = {}
-    discounted = 0
     
     for item in on_offer:
         offer_quantity = discounts[item][0]
         customer_quantity = skus[item]
         to_discount[item] = customer_quantity//offer_quantity
         normal_price[item] = customer_quantity%offer_quantity
+
+    for item in to_discount:
+        full_price += to_discount[item] * discounts[item][0]
     
     # skus are updated to only contain excess quantities that didn't fit in offer
     skus = skus | normal_price
@@ -51,5 +46,6 @@ def checkout(skus:str)->int:
             # scenario where an invalid value is entered
             return -1
     return full_price
+
 
 
