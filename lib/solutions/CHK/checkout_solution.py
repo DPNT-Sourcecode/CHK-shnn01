98c64455189f23e88a5f40c37aa76f1ec9a41b67
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 10:49:52 +0000

    Thu Apr 04 10:49:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -67,7 +67,9 @@ Our price table and offers:
 
 """
 from collections import Counter
-prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40, "F":10, "G":20, "H":10, "I":35, "J":60, "K":80, "L":90, "M":15, ""}
+
+prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40, "F":10, "G":20, "H":10, "I":35, "J":60, "K":80, "L":90, 
+          "M":15, "N":40, "O":10, "P":50, "Q":30, "R":50, "S":30, "T":20, "U":40, "V":50, "W":20, "X":90, "Y":10, "Z":50}
 bundles = {"E":[2, {"B":1}], "F":[3, {"F":1}]}
 discounts = {"A": {5:200, 3:130}, "B": {2:45}, }
 
@@ -120,4 +122,5 @@ def checkout(skus:str)->int:
     return full_price
 
 checkout("FFFF")
+
 
