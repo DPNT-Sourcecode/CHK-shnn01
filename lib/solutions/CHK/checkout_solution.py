7cd52deae09d7ca0dbb24ccbbe8a3d68b7f87b54
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 06:50:57 +0000

    Thu Apr 04 06:50:57 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -18,20 +18,23 @@ from collections import Counter
 prices = {"A": 50, "B": 30, "C": 20, "D": 15,}
 discounts = {"A": (3, 130), "B": (2, 45), }
 
+def apply_discount(on_offer:dict):
+    for item in on_offer
+
+
 def checkout(skus:str)->int:
     full_price = 0
 
     skus = Counter(skus)
     for item in skus:
         if item in discounts and (skus[item]%discounts[item][0]==0):
-            # caveat: this puts the condition that the discount will only be applied if the ordered quantity is a perfect multiple of 
-            # of the quantity specified in the discount. 
             item_group_count = skus[item] / discounts[item][0]
             item_price = item_group_count * discounts[item][0]
             full_price += item_price
         elif item in prices:
             full_price += skus[item] * prices[item]
         else:
-            # scenario where no valid value is entered
+            # scenario where an invalid value is entered
             return -1
     return full_price
+
