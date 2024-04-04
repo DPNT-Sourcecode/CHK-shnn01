fe1bccb47a1605af30c77a81a5febf1b73a2e97a
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 10:52:52 +0000

    Thu Apr 04 10:52:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -70,8 +70,8 @@ from collections import Counter
 
 prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40, "F":10, "G":20, "H":10, "I":35, "J":60, "K":80, "L":90, 
           "M":15, "N":40, "O":10, "P":50, "Q":30, "R":50, "S":30, "T":20, "U":40, "V":50, "W":20, "X":90, "Y":10, "Z":50}
-bundles = {"E":[2, {"B":1}], "F":[3, {"F":1}]}
-discounts = {"A": {5:200, 3:130}, "B": {2:45}, }
+bundles = {"E":[2, {"B":1}], "F":[3, {"F":1}], "N":[3, {"M":1}], "R":[3, {"Q":1}],}
+discounts = {"A": {5:200, 3:130}, "B": {2:45}, "H": {10:80, 5:45}, "K": {2:150}, "P": {5:200}, "Q": {3:80},}
 
 def apply_bundles(skus:str):
     bundled = {item:skus[item] for item in skus if item in bundles}
@@ -122,5 +122,6 @@ def checkout(skus:str)->int:
     return full_price
 
 checkout("FFFF")
+
 
 
