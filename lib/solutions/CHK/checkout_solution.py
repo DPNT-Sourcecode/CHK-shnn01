5067676e38919e06dc7b003f77645279379beb16
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 10:33:54 +0000

    Thu Apr 04 10:33:54 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -23,10 +23,21 @@ Our price table and offers:
 | E    | 40    | 2E get one B free      |
 +------+-------+------------------------+
 
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
++------+-------+------------------------+
+
 """
 from collections import Counter
 prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40}
-bundles = {"E":[2, {"B":1}]}
+bundles = {"E":[2, {"B":1}], "F":[2, {"F":1}]}
 discounts = {"A": {5:200, 3:130}, "B": {2:45}, }
 
 def apply_bundles(skus:str):
@@ -76,6 +87,3 @@ def checkout(skus:str)->int:
             # scenario where an invalid value is entered
             return -1
     return full_price
-
-result = checkout("EE")
-print(result)
