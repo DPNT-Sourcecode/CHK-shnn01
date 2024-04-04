0e17d9ce8b58955f01ab935566a3223b0166c75e
sbx_user1051 <sbx_user1051@169.254.37.149>
2024-04-04 10:46:52 +0000

    Thu Apr 04 10:46:52 UTC 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -34,9 +34,40 @@ Our price table and offers:
 | F    | 10    | 2F get one F free      |
 +------+-------+------------------------+
 
++------+-------+------------------------+
+| Item | Price | Special offers         |
++------+-------+------------------------+
+| A    | 50    | 3A for 130, 5A for 200 |
+| B    | 30    | 2B for 45              |
+| C    | 20    |                        |
+| D    | 15    |                        |
+| E    | 40    | 2E get one B free      |
+| F    | 10    | 2F get one F free      |
+| G    | 20    |                        |
+| H    | 10    | 5H for 45, 10H for 80  |
+| I    | 35    |                        |
+| J    | 60    |                        |
+| K    | 80    | 2K for 150             |
+| L    | 90    |                        |
+| M    | 15    |                        |
+| N    | 40    | 3N get one M free      |
+| O    | 10    |                        |
+| P    | 50    | 5P for 200             |
+| Q    | 30    | 3Q for 80              |
+| R    | 50    | 3R get one Q free      |
+| S    | 30    |                        |
+| T    | 20    |                        |
+| U    | 40    | 3U get one U free      |
+| V    | 50    | 2V for 90, 3V for 130  |
+| W    | 20    |                        |
+| X    | 90    |                        |
+| Y    | 10    |                        |
+| Z    | 50    |                        |
++------+-------+------------------------+
+
 """
 from collections import Counter
-prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40, "F":10}
+prices = {"A": 50, "B": 30, "C": 20, "D": 15, "E":40, "F":10, "G":20, "H":10, "I":35, "J":60, "K":80, "L":90, "M":15, ""}
 bundles = {"E":[2, {"B":1}], "F":[3, {"F":1}]}
 discounts = {"A": {5:200, 3:130}, "B": {2:45}, }
 
@@ -89,3 +120,4 @@ def checkout(skus:str)->int:
     return full_price
 
 checkout("FFFF")
+
